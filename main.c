7bd262ca6ff05dede592e186487a6c1428364c8c
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-12 15:29:57 +0200

    experimenting with function pointers on State_A I just created

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-void funMyfun()
+void State_A_Enter()
 {
-     printf("Hello World");
+     printf("State_A_Entered\n");
 }
 
+
+void State_A_loop()
+{
+     printf("State_B_looping\n");
+}
+
+
+void State_A_output()
+{
+     printf("State_B_output\n");
+}
 
 typedef void (*f_pointer_void_void)(void);
 
@@ -12,8 +23,12 @@ typedef void (*f_pointer_void_void)(void);
 int main()
 {
     
-   f_pointer_void_void p  = funMyfun;
-   (*p) ();
+    f_pointer_void_void p  = State_A_Enter;
+    (*p) ();
+    p = State_A_loop;
+    (*p) ();
+    
+    
 
     return 0;
 }
