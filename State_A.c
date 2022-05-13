c01a4cb679d741e04aed3d3a1a1b3e5d1172d1d8
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-13 03:24:45 +0200

    organizing the code more and spliting the definition of the great State Struct into small blocks in each State file

--- State_A.c
+++ State_A.c
@@ -17,3 +17,11 @@ void State_A_Output()
 {
      printf("State_A_output\n");
 }
+
+void State_A_Init()
+{
+     myStates.State_A.Enter  = State_A_Enter;
+     myStates.State_A.Loop   = State_A_Loop;
+     myStates.State_A.Output = State_A_Output;
+     loop[State_A] = myStates.State_A.Loop;
+}
