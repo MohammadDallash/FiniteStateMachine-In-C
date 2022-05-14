c3aad25d982a398e29d35c05e4c0a007d0430579
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-14 16:22:29 +0200

    adding an important comment

--- FSMmanager.c
+++ FSMmanager.c
@@ -14,12 +14,11 @@ States myStates;
 void FSM_int()
 {
     Update = (f_pointer_void_void*)calloc(NOfStates, sizeof(f_pointer_void_void));
+    /*we cannot free the memory we allocated for the array as we are using it throughout
+    the whole program which is running consciously as we are jumping from a state to another one*/
     State_A_Init();
     State_B_Init();
 
-     
-    
-   
 };
 
 StatesEnum CurretState;
